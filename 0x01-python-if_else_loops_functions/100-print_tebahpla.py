86eb2b57b2ae0dcf05a163f71c583894ad82f840
karllucas <karlkamakia@gmail.com>
2022-05-31 15:33:52 -0700

    submit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for alphabet in reversed(range(97, 123)):
-    if alphabet % 2 == 0:
-        print("{:c}".format(alphabet)
-    else:
-        print("{:c}".format(alphabet - 32)), end='')
+    print("{:c}".format(alphabet if (alphabet % 2 == 0) else (alphabet - 32)), end='')
