da7a3712157e7bfe1f3350142fa145f2179b3dd6
karllucas <karlkamakia@gmail.com>
2022-05-31 12:02:02 -0700

    submit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for alphabet in range(97, 123):
-    if alphabet == 113 || alphabet == 101
-         break;
+    if alphabet == 113:
+        break
+    elif alphabet == 101:
+        break
     else:
         print("{:c}".format(alphabet), end='')
