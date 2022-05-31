258ac22cc368c340055e2dbe477dabca763bb4fb
karllucas <karlkamakia@gmail.com>
2022-05-31 15:46:43 -0700

    submit

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    copied = ''
+    for letter in range(len(str)):
+        if letter != n:
+            copied = copied + str[i]
+    return (copied)
