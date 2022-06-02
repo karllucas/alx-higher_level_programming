510043c929589cc8b381f71ca8e2c2b2a762b4d3
karllucas <karlkamakia@gmail.com>
2022-06-02 05:14:53 -0700

    submit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     sum = 0
-    for argument in range(1, len(argv)):
-        sum += int(argv[arguement])
-    print("{}".format(arguement))
+    for j in range(1, len(argv)):
+        sum += int(argv[j])
+    print("{}".format(j))
