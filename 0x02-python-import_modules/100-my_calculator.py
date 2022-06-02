674c5e70a30a73d32aa862eb2e735212ebd8e199
karllucas <karlkamakia@gmail.com>
2022-06-02 14:44:12 -0700

    submit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,13 @@
 if __name__ == "__main__":
     import sys
 
-    num_args = len(sys.argv) - 1
-    if num_args != 3:
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     op = sys.argv[2]
-     if op != '+' and op != '-' and op != '*' and op != '/':
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
