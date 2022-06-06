af7defc7a4deb375dd49f60383393661e4e62919
karllucas <karlkamakia@gmail.com>
2022-06-06 11:41:02 -0700

    submit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    [[row[i] for row in matrix] for i in range(3)]
+    transposed = []
+    for i in range(3):
+        transposed.append([row[i] for row in matrix])
+    print('{}'.format(transposed))
