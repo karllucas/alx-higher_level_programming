a375ce668f8d974d5e8c08fb314bff22f5e9f55a
karllucas <karlkamakia@gmail.com>
2022-06-20 12:34:34 -0700

    QO correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return True
     except:
-        return (False)
-    else:
-        return (True)
+        return False
