5503a30b02d6a4f9852be8219d580c91c1c064c5
karllucas <karlkamakia@gmail.com>
2022-06-21 13:40:36 -0700

    submit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+# 2-square.py
 """Define a class Sqaure."""
+
 class Sqaure:
     """Represent a sqaure."""
+
     def __init__(self, size=0):
         """Initialize a new Square.
         Args:
             size (int): The size of the new square.
         """
-        try:
-            self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
