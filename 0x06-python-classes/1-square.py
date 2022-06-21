45b114f9a10d0e224e96de478ce718506d0d7248
karllucas <karlkamakia@gmail.com>
2022-06-21 12:36:36 -0700

    submit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+class Sqaure:
+    """Class Square that defines a square"""
+    def __init__(self, size):
+        """Initializes data represemted by size parameter"""
+        self.__size = size
