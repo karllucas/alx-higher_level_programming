17c1445d3ef8c59ce867b8dd3e8f8f2eb6eb6ab5
karllucas <karlkamakia@gmail.com>
2022-06-21 13:53:09 -0700

    submit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,15 +4,17 @@
 
 class Sqaure:
     """Represent a sqaure."""
-
     def __init__(self, size=0):
-        """Initializes the data."""
+        """ Method to initialize the square object"""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
-        """Returns the current square area."""
-        return self.__size *= self.__size
+        """ Method that returns the square are of the object"""
+
+    return (self.__size ** 2)
