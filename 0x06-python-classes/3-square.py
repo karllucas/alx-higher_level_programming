64cd1c836d305a48604d84c0b0f2fea5474b2f05
karllucas <karlkamakia@gmail.com>
2022-06-21 13:48:42 -0700

    submit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 3-square.py
+"""Define a class Sqaure."""
+
+class Sqaure:
+    """Represent a sqaure."""
+
+    def __init__(self, size=0):
+        """Initializes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size *= self.__size
