89664665360ec04775935bba56401c3ea2469170
karllucas <karlkamakia@gmail.com>
2022-06-21 14:01:21 -0700

    submit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,12 +2,12 @@
 # 2-square.py
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new Square.
-
         Args:
             size (int): The size of the new square.
         """
