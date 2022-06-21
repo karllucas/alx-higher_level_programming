952b167663c8a53cac494686cb743d5146eea9b3
karllucas <karlkamakia@gmail.com>
2022-06-21 12:03:13 -0700

    submit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
+root@82534809e2ff:~/alx-higher_level_programming/0x06-python-classes# cat > 0-square.py
 #!/usr/bin/python3
 class Square:
     pass # An empty block
 
     s = Square()
-    print(my_square)
+    print(s)
