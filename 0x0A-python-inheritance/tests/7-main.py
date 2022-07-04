7af2faa07fb296ca43229821455f11b9090e194a
karllucas <karlkamakia@gmail.com>
2022-07-04 15:25:57 -0700

    submit

--- 0x0A-python-inheritance/tests/7-main.py
+++ 0x0A-python-inheritance/tests/7-main.py
@@ -20,13 +20,3 @@ try:
     bg.integer_validator("distance", -4)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricin
